1d177f2adf07f2f8ef7ec8c6600432e7995c6fae
180150 <180150@PCI42102>
2022-12-02 14:14:48 +0100

    Ajoutez des fichiers projet.

---
+++ ACT4POO/Cercle1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACT4POO
+{
+    class Cercle1
+    {
+        private double rayon;
+        private bool ok = false;
+        private char retry;
+        private double air = 0;
+        private double per = 0;
+
+        public void CalculerAire()
+        {
+            
+            
+            while (ok != true)
+            {
+                {
+                    Console.WriteLine("Entrer l'air :");
+                    rayon = double.Parse(Console.ReadLine());
+                    air = calcAir(rayon);
+                    per = calcPer(rayon);
+                    ok = true;
+                    Console.WriteLine($"Le cecle de rayon {rayon} a un périmètre {per} et une aire {air}. Avec un cercle de rayon");
+
+
+
+
+                }
+            }
+
+            {
+
+            }
+
+              
+            
+
+        }
+        private double calcAir(double rayon)
+        {
+            return Math.PI * (rayon * rayon);
+        }
+
+        private double calcPer(double rayon)
+        {
+            return rayon * Math.PI;
+        }
+    }
+}
+}
---
+++ ACT4POO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACT4POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
